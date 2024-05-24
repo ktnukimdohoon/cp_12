298935346061bf711442f69b6313a470f7836853
ktnukimdohoon <163504699+ktnukimdohoon@users.noreply.github.com>
2024-05-24 16:27:42 +0900

    12주차 퀴즈
    퀴즈

---
+++ Quiz.py
@@ -0,0 +1,18 @@
+class 붕어빵:
+    def __init__(self, taste, price):
+        self.taste = taste
+        self.price = price
+        self.total = 0
+    def sell(self):
+        print(f"{self.taste}을 {self.price}에 팔았습니다.")
+        self.total += self.price
+    def eat(self):
+        print(f"{self.taste}을 먹습니다.")
+
+
+슈크림_붕어빵 = 붕어빵("슈크림", 1000)
+슈크림_붕어빵.sell()
+슈크림_붕어빵.sell()
+슈크림_붕어빵.sell()
+슈크림_붕어빵.sell()
+print(슈크림_붕어빵.total)
